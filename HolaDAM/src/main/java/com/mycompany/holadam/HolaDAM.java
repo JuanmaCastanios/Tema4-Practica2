76420051bb72a0e24ab56a73a1b1623641cd4397
Juan Jose <juanjoseblancodiaz1504@gmail.com>
2024-01-23 17:57:30 +0100

    Modificacion Juan

--- HolaDAM/src/main/java/com/mycompany/holadam/HolaDAM.java
+++ HolaDAM/src/main/java/com/mycompany/holadam/HolaDAM.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.holadam;
 
 /**
@@ -16,6 +11,6 @@ public class HolaDAM {
         
         System.out.println("Estoy editando");
         
-        System.out.println("Modificandolo de nuevo (punto 3)");        
+        System.out.println("Durante el punto 3, modifico el archivo");        
     }
 }
